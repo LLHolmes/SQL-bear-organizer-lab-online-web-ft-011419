6ad1ecbd06332671198286e3b591ce67972505b3
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-12 16:27:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,7 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "select * from bears where name = 'NULL';"
+  "SELECT * FROM bears WHERE name = 'NULL';"
 end
 
 # id, name, age, gender, color, temperament, alive
